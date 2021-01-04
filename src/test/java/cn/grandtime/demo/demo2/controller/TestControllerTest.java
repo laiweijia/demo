aa6dedc966099f693a62a8e5b23a23aa3ebbb724
赖伟佳 <a@b.c>
2021-01-04 13:57:30 +0800

    .

--- src/test/java/cn/grandtime/demo/demo2/controller/TestControllerTest.java
+++ src/test/java/cn/grandtime/demo/demo2/controller/TestControllerTest.java
@@ -6,11 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
