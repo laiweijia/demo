41fe4a5ad5d9c273d487a603f8b55758ec996bda
L <L@abc.com>
2023-07-27 10:14:03 +0800

    精简配置

--- src/main/java/cn/grandtime/demo/demo0/controller/TestController.java
+++ src/main/java/cn/grandtime/demo/demo0/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.grandtime.demo.demo0.controller;
 
 import cn.grandtime.demo.demo0.service.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
  *
  * @author 赖伟佳
  */
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
